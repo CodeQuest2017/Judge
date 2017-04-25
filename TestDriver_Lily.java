d7e4dbdddecffedbee35f8c7a3259e112263c32d
Donovan Glover <UltimateDonovan@gmail.com>
2017-04-25 13:18:25 -0400

    Remove legacy code

--- TestDriver_Lily.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-import java.io.*;
-
-public class TestDriver_Lily {
-	public static void main(String[] args) throws Exception {
-		System.out.println("var data = " + ParseJson.parseJson("data.js").toString());
-	}
-}
