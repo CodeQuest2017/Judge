533e8eebc112d60133108e0d798b2ee1c7ed865f
Donovan Glover <UltimateDonovan@gmail.com>
2017-04-25 12:57:15 -0400

    Add our initial abstraction of a Problem

---
+++ Problem.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Problem{
+    private Map<String, String> map;
+    private String out;
+    
+    public Problem(Map<String, String> map){
+        this.setMap(map);
+    }
+    public Map<String, String> getMap(){
+        return this.map;
+    }
+    public void setMap(Map<String, String> map){
+        this.map = map;
+    }
+    public String toJson(){
+        out="{\n";
+        this.map.forEach((k, v) -> {
+            out+="\t\""+k+"\""+": "+v+",\n";
+        });
+        return out+"}";
+    }
+    public String toString(){
+        return this.toJson();
+    }
+    
+}
+// [ {}, {}, ]
